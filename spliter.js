5c85db7572136cdca333913df8ba0b8287ffcfa5
S Y A U Q I <57295427+syauqqii@users.noreply.github.com>
2022-12-29 11:03:23 +0700

    Create spliter.js
    File ini digunakan untuk memecah 1 file menjadi beberapa bagian, dengan menggunakan program ini juga dapat memepercepat melakukan teknik bruteforece dengan cara split file menjadi beberapa bagian lalu di run secara bersamaan menggunakan program bruteforce yang kalian miliki.

---
+++ spliter.js
@@ -0,0 +1,96 @@
+/* Program ini digunakan untuk split 1 file menjadi banyak file
+ * -> return dari program ini akan menghasil kan folder 'result'
+ *    isi dari folder 'result' adalah hasil dari split beberapa file,
+ *    tergantung jumlah yang anda inputkan didalam split file.
+ */
+
+const red    = "\x1b[31m";
+const yellow = "\x1b[33m";
+const green  = "\x1b[32m";
+const cyan   = "\x1b[36m";
+const white  = "\x1b[37m";
+
+const args = process.argv;
+if(args.length <= 3){
+	console.log(`\n ${cyan}[${white}+${cyan}] Cara penggunaan${white}:${white} node spliter.js {jumlah_hasil_file} {nama_file}`);
+	console.log(`          ${cyan}Contoh    ${white}: node spliter.js 5 data.txt`);
+	process.exit();
+}
+
+const split = args[2];
+if(split < 2){
+	console.log(`\n ${yellow}[${white}!${yellow}] INFO${white}: lawak dek? split 1 file maksudnya gimana? >:(`);
+	console.log(`\n ${cyan}[${white}+${cyan}] Cara penggunaan${white}: node spliter.js {jumlah_hasil_file} {nama_file}`);
+	console.log(`          ${cyan}Contoh    ${white}: node spliter.js 5 data.txt`);
+	process.exit();
+}
+
+const filename  = args[3].split(".");
+const folderName = 'result';
+
+const fs = require("fs");
+
+try {
+	if (!fs.existsSync(folderName)) {
+		fs.mkdirSync(folderName);
+	}
+} catch (err) {
+	console.error(err);
+}
+
+try{
+	const contents = fs.readFileSync(`${filename[0]}.${filename[1]}`, "utf-8");
+	const array    = contents.split(/\r?\n/);
+	const lines    = array.length;
+
+	if(lines % split == 0){
+		let formula  = lines / split;
+
+		for(let i=1; i<=split; i++){
+			for(let j=formula*(i-1); j<(formula*i); j++){
+				if(j==(formula*i)-1){
+					fs.appendFile(`result/${filename[0]}_${i}.${filename[1]}`, `${array[j]}`, function (err) {
+						if(err) throw err;
+					});
+				} else{
+					fs.appendFile(`result/${filename[0]}_${i}.${filename[1]}`, `${array[j]}\n`, function (err) {
+						if(err) throw err;
+					});
+				}
+			}
+		}
+	} else{
+		let temp    = lines % split;
+		let formula = (lines-temp) / split;
+
+		for(let i=1; i<=split; i++){
+			for(let j=formula*(i-1); j<(formula*i); j++){
+				if(j==(formula*i)-1){
+					fs.appendFile(`result/${filename[0]}_${i}.${filename[1]}`, `${array[j]}`, function (err) {
+						if(err) throw err;
+					});
+				} else{
+					fs.appendFile(`result/${filename[0]}_${i}.${filename[1]}`, `${array[j]}\n`, function (err) {
+						if(err) throw err;
+					});
+				}
+			}
+		}
+
+		/* BUG */
+		for(let i=split+1; i<=split+1; i++){
+			for(let j=(formula-temp+1)*(i-1); j<((formula-temp+1)*i); j++){
+				fs.appendFile(`result/${filename[0]}.sisa.${filename[1]}`, `${array[j]}\n`, function (err) {
+					if(err) throw err;
+				});
+			}
+		}
+	}
+
+	console.log(`\n ${green}[${white}+${green}] SUKSES${white}: file '${filename[0]}.${filename[1]}' berhasil displit menjadi '${split}' file!`);
+} catch(err){
+	console.log(`\n ${red}[${white}!${red}] ERROR${white}: file '${filename[0]}.${filename[1]}' tidak ditemukan!! >:(`);
+	console.log(`\n ${cyan}[${white}+${cyan}] Cara penggunaan${white}: node spliter.js {jumlah_hasil_file} {nama_file}`);
+	console.log(`          ${cyan}Contoh    ${white}: node spliter.js 5 data.txt`);
+	process.exit();
+}
